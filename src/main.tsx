d01df6f67d2264d85de734aa0b57b50c757ed172
nuyhv <volumefd@gmail.com>
2023-09-27 20:46:03 +0900

    :sparkles: Globalstyle 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
