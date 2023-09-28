e2e94e1495fe95ffb8d0be0ed30af904c9f1e665
nuyhv <volumefd@gmail.com>
2023-09-28 20:54:35 +0900

    :sparkles: Project 컴포넌트화

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,10 +1,40 @@
-import { ContentWrapper } from "../../assets/styles/ContentWrapper.tsx";
+import { ContentWrapper, Tag } from "../../assets/styles/CommonStyles.tsx";
+import styled from "styled-components";
+import { projectScripts } from "../../scripts/projects/preview.ts";
+
+const ImgContainer = styled.div`
+  width: 100%;
+  height: 20rem;
+  overflow: hidden;
+  border-radius: 1rem;
+  img {
+    object-fit: cover;
+    transition: 0.3s ease;
+    &:hover {
+      scale: 1.03;
+    }
+  }
+`;
 
 const Projects = () => {
   return (
-    <ContentWrapper id="projects">
-      <h1>Projects</h1>
-    </ContentWrapper>
+    <section id="projects" className="flex flex-col gap-8">
+      {projectScripts.map((project) => (
+        <ContentWrapper key={project.id} data-aos="fade-up">
+          <h1>Projects</h1>
+          <ImgContainer>
+            <img src={project.image} alt={project.title} />
+          </ImgContainer>
+          <h2 className="text-2xl font-semibold py-2">{project.title}</h2>
+          <div>
+            {project.skills.map((skills) => (
+              <Tag key={skills}>{skills}</Tag>
+            ))}
+          </div>
+          <p>{project.subTitle}</p>
+        </ContentWrapper>
+      ))}
+    </section>
   );
 };
 
