0e684007d7c55c08e4ca59f7567f9c8d7d3abeee
nuyhv <volumefd@gmail.com>
2023-09-28 18:54:33 +0900

    :sparkles: Lenis 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import * as React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Mainpage from "./pages/Mainpage";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  React.useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+    });
+  }, []);
+
   return (
     <>
       <Header />
