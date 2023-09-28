f3b277504460705f9d56e1327280ca14069cff69
nuyhv <volumefd@gmail.com>
2023-09-28 20:53:44 +0900

    :lipstick: 반응형 조건 추가

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -5,15 +5,19 @@ const InfoWrapper = styled.div`
   height: 36rem;
   padding: 3rem 1.6rem;
   position: sticky;
-  top: 83.5px;
+  top: 79.8px;
   background-color: #485276;
   color: white;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Info = () => {
   return (
     <InfoWrapper>
       <h1>Introduce</h1>
+      <p>여기에 깃헙이라던가 블로그라던가 연락처 들어감</p>
     </InfoWrapper>
   );
 };
