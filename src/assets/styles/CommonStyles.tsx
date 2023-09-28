634682e6de0be5fcbdb877a86a87db53a4af21f9
nuyhv <volumefd@gmail.com>
2023-09-28 18:53:59 +0900

    :truck: 공통 스타일로 병합

---
+++ src/assets/styles/CommonStyles.tsx
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const ContentWrapper = styled.section`
+  min-height: 36rem;
+  padding: 3rem;
+  border-radius: 1rem;
+  box-shadow: 0 0 1rem 0.5rem rgba(0, 0, 0, 0.1);
+  background-color: white;
+`;
+
+export const Tag = styled.span`
+  display: inline-block;
+  padding: 0.2rem 0.5rem;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  border-radius: 1rem;
+  border: 2px solid #485276;
+  color: #485276;
+  font-size: 0.8rem;
+  font-weight: 600;
+`;
