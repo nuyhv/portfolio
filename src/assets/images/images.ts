ba1ed550f610bd0dfb5d9b3ba9c4affc3350b6dc
nuyhv <volumefd@gmail.com>
2023-09-29 21:22:49 +0900

    :sparkles: 아이콘 추가

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,11 @@
-import bbangorder from "../assets/images/bbangorder.png";
+import bbangorder from "./bbangorder.png";
+import gmail from "./gmail.svg";
+import github from "./gmail.svg";
 
 const images = {
   bbangorder,
+  gmail,
+  github,
 };
 
 export default images;
