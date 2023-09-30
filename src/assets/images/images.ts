599a40e5ea15d86b5dd2bfe32f50615b28031230
nuyhv <volumefd@gmail.com>
2023-10-01 00:45:45 +0900

    :sparkles: Skill 이미지 추가

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -4,6 +4,8 @@ import github from "./github.svg";
 import phone from "./phone.svg";
 import velog from "./velog.svg";
 import icebear from "./icebear.png";
+import stackclone from "./stackoverflow.png";
+import zustand from "./zustand.png";
 
 const images = {
   bbangorder,
@@ -12,6 +14,8 @@ const images = {
   phone,
   velog,
   icebear,
+  stackclone,
+  zustand,
 };
 
 export default images;
