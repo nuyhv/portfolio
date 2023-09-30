94e8bb8af8ec4fa5a451a130cb3c755c0791de3d
nuyhv <volumefd@gmail.com>
2023-10-01 00:47:26 +0900

    :fire: 불필요한 주석 삭제

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,7 +1,3 @@
-// declare module "*.svg" {
-//   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-//   export default content;
-// }
 declare module "*.svg" {
   import React = require("react");
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
