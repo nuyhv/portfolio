579ec2c83970825be2921bc80970a8b1b08ecc27
nuyhv <volumefd@gmail.com>
2023-09-30 20:45:42 +0900

    :sparkles: 이미지 모듈화

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,11 +1,17 @@
 import bbangorder from "./bbangorder.png";
 import gmail from "./gmail.svg";
-import github from "./gmail.svg";
+import github from "./github.svg";
+import phone from "./phone.svg";
+import velog from "./velog.svg";
+import icebear from "./icebear.png";
 
 const images = {
   bbangorder,
   gmail,
   github,
+  phone,
+  velog,
+  icebear,
 };
 
 export default images;
