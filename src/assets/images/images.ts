ee6603023c2d0d089c9ee610509a31676bbdf30b
nuyhv <volumefd@gmail.com>
2023-10-07 23:17:11 +0900

    :sparkles: 프리뷰 이미지 추가

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,22 +1,28 @@
 import bbangorder from "./bbangorder.png";
+import bbangorder2 from "./bbangorder2.png";
+import bbangorder3 from "./bbangorder3.png";
 import gmail from "./gmail.svg";
 import github from "./github.svg";
 import phone from "./phone.svg";
 import velog from "./velog.svg";
 import icebear from "./icebear.png";
 import stackclone from "./stackoverflow.png";
+import stackclone2 from "./stackoverflow2.png";
 import zustand from "./zustand.png";
 import ribbon from "./ribbon.svg";
 import linkedin from "./linkedin.svg";
 
 const images = {
   bbangorder,
+  bbangorder2,
+  bbangorder3,
   gmail,
   github,
   phone,
   velog,
   icebear,
   stackclone,
+  stackclone2,
   zustand,
   ribbon,
   linkedin,
