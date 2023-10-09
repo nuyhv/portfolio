e93b9856cb08c7c828b89cc6b7d9b7c6da2e16e9
nuyhv <volumefd@gmail.com>
2023-10-09 23:13:30 +0900

    :sparkles: About 페이지 매핑

--- src/pages/aboutme/Aboutme.tsx
+++ src/pages/aboutme/Aboutme.tsx
@@ -1,10 +1,53 @@
 import { ContentWrapper } from "../../assets/styles/CommonStyles.tsx";
+import { aboutScripts } from "../../scripts/projects/preview.ts";
+import { styled } from "styled-components";
+
+const Introduce = styled.ul`
+  padding-top: 1rem;
+`;
+
+const Profile = styled.div`
+  min-width: 250px;
+  max-width: 500px;
+  height: 300px;
+  border-radius: 1rem;
+  background-color: gray;
+  overflow: hidden;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  gap: 1.5rem;
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+  .highlight {
+    font-weight: 700;
+    background-clip: border-box;
+    background: -webkit-linear-gradient(#eee, #333);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
 
 const Aboutme = () => {
   return (
     <ContentWrapper id="aboutme">
       <h1>About me</h1>
-      <p>내소개 어쩌고저쩌고</p>
+      <Contents>
+        <Profile>dd</Profile>
+        <div>
+          <h2>{aboutScripts.title}</h2>
+          <Introduce>
+            {aboutScripts.desc.map((desc) => (
+              <li className="pb-4">{desc}</li>
+            ))}
+          </Introduce>
+        </div>
+      </Contents>
     </ContentWrapper>
   );
 };
