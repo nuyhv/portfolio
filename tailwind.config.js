7a9575cbec69efbde44f3ee9cc14d41333e9d191
nuyhv <volumefd@gmail.com>
2023-10-13 21:18:22 +0900

    :sparkles: Tailwind custom font 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      opensans: ["Open Sans", "sans-serif"],
+      notosans: ["Noto Sans KR", "sans-serif"],
+    },
   },
   plugins: [],
 };
