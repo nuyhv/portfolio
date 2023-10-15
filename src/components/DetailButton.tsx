e5e01201dc1c03b6ac0c7a057051b74db959def4
nuyhv <volumefd@gmail.com>
2023-10-16 02:12:01 +0900

    :lipstick: Detail button 레이아웃 변경

--- src/components/DetailButton.tsx
+++ src/components/DetailButton.tsx
@@ -1,14 +1,24 @@
 import { styled } from "styled-components";
+import images from "../assets/images/images";
 
 const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  width: max-content;
   border-radius: 1.5rem;
   background-color: #485276;
   color: white;
-  padding: 0.2rem 0.5rem;
-  font-size: 0.8rem;
-  font-weight: 500;
+  padding: 0.4rem 0.8rem;
+  font-size: 0.9rem;
+  cursor: pointer;
 `;
 
 export const DetailButton = () => {
-  return <Button>버튼</Button>;
+  return (
+    <Button>
+      <img src={images.magnifier} alt="More Button" width="20" />
+      자세히 보기
+    </Button>
+  );
 };
